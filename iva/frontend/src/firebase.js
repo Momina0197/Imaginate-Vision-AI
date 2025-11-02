0a51a2165434daaecd78d2befa013596b292f0bc
Momina0197 <mominatariq58@gmail.com>
2025-11-02 09:57:44 -0800

    deploy

--- iva/frontend/src/firebase.js
+++ iva/frontend/src/firebase.js
@@ -3,12 +3,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD9G9MuRvdKJB7soXNeBNT5RU3oa6A6Alk", // paste from Firebase project settings
-  authDomain: "imaginate-vision-ai-90182.firebaseapp.com",
-  projectId: "imaginate-vision-ai-90182",
-  storageBucket: "imaginate-vision-ai-90182.firebasestorage.app",
-  messagingSenderId: "799517757998",
-  appId: "1:799517757998:web:812a45a02dbfcd180585bf"
+  apiKey: "AIzaSyDEZbFPwkX-HiQz1IFb5TPlf9FZD7v9Ak0",
+  authDomain: "imaginate-vision-ai.firebaseapp.com",
+  projectId: "imaginate-vision-ai",
+  storageBucket: "imaginate-vision-ai.firebasestorage.app",
+  messagingSenderId: "596149403121",
+  appId: "1:596149403121:web:3c4c309fad6a1d97495850",
+  measurementId: "G-S749LDBY4H"
 };
 
 const app = initializeApp(firebaseConfig);
